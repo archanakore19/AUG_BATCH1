a908213896c0b0207f2b4049c7f7493fc5362b45
archanakore19 <archanakore1905@gmail.com>
2023-12-13 14:11:20 +0530

    modify javascript

---
+++ JavaScript/variable.js
@@ -0,0 +1,19 @@
+// variable: are the container to store data (data type)
+// let/ var/const default var(keywords) followed by VN
+// VN: rule >>> space not allowed.
+// first letter of variable should always alphabets,
+// Special characters not allowed ( except $_)
+
+let fName; // declaration of variable // undefined
+fName='Archana'; //defination (syntax variableName = value/data) // difference = == ===
+
+let myFirstName = 'Rani'; // declaration + defination
+var mySName='Kore';
+var myName;
+myName='Virpaksh';
+myName='pooja';
+
+
+const cityName='Solapur';  // const keyword intialization of value is must
+// city='pune';
+console.log('cityName',cityName);
